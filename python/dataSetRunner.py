b1775e199ba160d0d6e2cca05895a691723a7300
Xunjun Yin <yinxunjun@126.com>
2020-07-05 22:38:16 +0800

    Create dataSetRunner.py

---
+++ python/dataSetRunner.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+import yaml
+import json
+import requests
+
+yaml_config = """
+# todo:
+# * supports string type parameters, e.g.:
+#   content: 12341,12342,12343,12344,12345,12346,12347,12348,12349,12340
+# * supports counter type parameters, e.g.:
+#   range: 0, 10, 1
+# * support regex response parser, e.g.:
+#   response_type: reg
+#   names: a, b, c
+#   pattern: a(\d+)b(\d+)c1(\w+)000
+# * support json fields multi-level parse
+# * support post method if requests has notnull data
+
+parameters:
+-
+  names:
+  - vid
+  - title
+  filename: /tmp/test
+  delimiter: ','
+
+requests:
+-
+  headers:
+    Cookie: xxxxx
+  url: https://domain.com/video/articles/global/search?docid=${vid}
+  names:
+  -
+    name: category
+    path: $..history[?(@.userid == "system")].category
+  -
+    name: keywords
+    path: $..history[?(@.userid == "system")].keywords
+  -
+    name: interests
+    path: $..history[?(@.userid == "system")].interests
+
+output: ${vid}, ${title}, ${category}, ${keywords}, ${interests}
+"""
+
+
+def http_json(path, data, headers):
+    try:
+        return json.loads(http_get(path, data, headers))
+    except Exception, e:
+        print "http_get failed for the path=%s, error=%s" % (path, e)
+        return json.loads('{}')
+
+
+def http_get(path, headers, timeout=30):
+    res = requests.get(path, headers=headers, timeout=timeout)
+    return res.content
+
+
+def http_post(path, data, headers, timeout=30):
+    res = requests.post(path, json.dumps(data), headers=headers, timeout=timeout)
+    return res.content
+
+
+def run():
+    params = {}
+    data = yaml.load(yaml_config, Loader=yaml.FullLoader)
+    print type(data)
+    print data
+
+run()
